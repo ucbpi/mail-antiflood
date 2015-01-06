487d846e55e339589f83def6781327de49d4bf38
Aaron Russo <arusso@berkeley.edu>
2015-01-06 12:39:47 -0800

    handle cases where nothing is returned but an empty string

--- hold_all_messages.sh
+++ hold_all_messages.sh
@@ -3,8 +3,10 @@ MAILQ=$(which mailq)
 cnt=0
 while read id
 do
-  postsuper -h $id
-  cnt=$(( $cnt + 1 ))
+  if [ "$id" != "" ]; then
+    postsuper -h $id
+    cnt=$(( $cnt + 1 ))
+  fi
 done <<<"$( $MAILQ | grep -E '^[A-Z0-9]{10}[^\!]' | awk '{ print $1 }' | sed 's/[\*]//' )"
 
 echo "held ${cnt} new messages"
