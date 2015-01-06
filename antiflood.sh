373e59cfca53c6b77768b09dd75331165989f286
Aaron Russo <arusso@berkeley.edu>
2015-01-06 13:47:40 -0800

    update name to antiflood

--- antiflood.sh
+++ antiflood.sh
@@ -9,14 +9,14 @@ These messages have been held to prevent rate-limiting of email accounts, and to
 To review these messages, hop on to $HOSTNAME and review the mailq, thawing or deleting messages as necessary."
 
 # load a config if necessary
-[[ -f "$DIR/antispam.config" ]] && . "$DIR/antispam.config"
+[[ -f "$DIR/antiflood.config" ]] && . "$DIR/antiflood.config"
 
 ACTIVE_COUNT=$(sh ${DIR}/check_postfix_queue_size.sh | sed 's/:/ /g' | awk '{print $4 }')
 if [ $ACTIVE_COUNT -ge $THRESHOLD ]; then
   # hold all messages
   # 
   ${DIR}/hold_active_messages.sh
-  printf "$MESSAGE" | mail -s "Nagios Anti-Spam System Activated" $NOTIFY
+  printf "$MESSAGE" | mail -s "Mail Anti-Flood System Activated" $NOTIFY
 else
   echo "message count below threshold ($ACTIVE_COUNT < $THRESHOLD)"
 fi
