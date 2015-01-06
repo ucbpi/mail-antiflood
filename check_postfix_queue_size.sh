36da3970bc26dc4980be3ecf7d1a9549c09b6560
Aaron Russo <arusso@berkeley.edu>
2015-01-06 13:05:09 -0800

    provide a bit more information about status of messages

--- check_postfix_queue_size.sh
+++ check_postfix_queue_size.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 MAILQ=$(which mailq)
+AWK="$(which awk) --re-interval"
 
-$MAILQ 2>/dev/null | grep -Ec '^[A-Z0-9]{10}'
+$MAILQ 2>/dev/null | $AWK '/^[A-Z0-9]{10}/ { total_count++ } /^[A-Z0-9]{10}[!]/ { held_count++ } END { print "total:"total_count,"active:"total_count-held_count,"held:"held_count }'
