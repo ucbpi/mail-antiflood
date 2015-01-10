d75c0629a0f8258e928344d42c4985a9fb95c910
Aaron Russo <arusso@berkeley.edu>
2015-01-09 16:05:38 -0800

    initialize held and total counts

--- check_postfix_queue_size.sh
+++ check_postfix_queue_size.sh
@@ -3,4 +3,4 @@
 MAILQ=$(which mailq)
 AWK="$(which awk) --re-interval"
 
-$MAILQ 2>/dev/null | $AWK '/^[A-Z0-9]{10}/ { total_count++ } /^[A-Z0-9]{10}[!]/ { held_count++ } END { print "total:"total_count,"active:"total_count-held_count,"held:"held_count }'
+$MAILQ 2>/dev/null | $AWK 'BEGIN { total_count=0; held_count=0; } /^[A-Z0-9]{10}/ { total_count++ } /^[A-Z0-9]{10}[!]/ { held_count++ } END { print "total:"total_count,"active:"total_count-held_count,"held:"held_count }'
